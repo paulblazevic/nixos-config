44710061e4a4aea1ddf6103d73ccf9746f5701b7
Paul Blazevic <paulblazevic@outlook.com>
2025-11-28 23:33:30 +1100

    Record setup – ready for 5 machines

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Paul's NixOS config – 5 identical machines";
+  description = "Paul's NixOS config for 5+ machines";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
@@ -10,19 +10,28 @@
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
-  let system = "x86_64-linux"; in
-  {
-    nixosConfigurations.paulsbox = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+  in {
+    nixosConfigurations = {
+      nixos1 = pkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.paul = import ./home.nix;
-        }
-      ];
+        }];
+      };
+      nixos2 = pkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.paul = import ./home.nix;
+        }];
+      };
+      # Add nixos3, nixos4, nixos5, etc. as needed
     };
   };
 }
